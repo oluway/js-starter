8649482adf9b3d161b4d01d32b81e5228f9cfc79
Ebenezer Olumide Ayeni <olu@timmalholdings.co.za>
2020-05-03 23:10:18 +0200

    creating tender services

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import app from './app';
 
+const verify = document.getElementById('verify');
+const section1 = document.getElementById('section1');
+const section2 = document.getElementById('section2');
+const section3 = document.getElementById('section3');
+
 const startApp = async () => {
   const header = document.querySelector('[data-app-name]');
   if (!header) return;
@@ -8,4 +13,18 @@ const startApp = async () => {
   header.textContent = programName;
 };
 
+const clickme = () => {
+  // eslint-disable-next-line no-console
+  section1.style.display = 'none';
+  section2.style.display = 'none';
+  section3.classList = 'loader';
+  setTimeout(() => {
+    section3.classList = 'noloader';
+    section2.style.display = 'inline';
+    section2.innerHTML = '<div class="text-center">Enter Tender Number/Reference <br><br> <input type="text" /><br> <input type="button" class="btn btn-primary" id="validate" value="Verify"/></div>';
+    section3.classList = 'olu';
+  }, 3000);
+};
+
 document.addEventListener('DOMContentLoaded', startApp);
+verify.addEventListener('click', clickme);
